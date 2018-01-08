41629f27995a1b877afd1d3bd8998266dd688f07
David Hall <david.hall@libertymutual.com>
2018-01-08 15:26:27 -0500

    initial commit of charicycle

---
+++ src/main/java/com/lmig/gfc/charicycle/CharicycleApplication.java
@@ -0,0 +1,12 @@
+package com.lmig.gfc.charicycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharicycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharicycleApplication.class, args);
+	}
+}
